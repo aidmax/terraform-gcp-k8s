3d213dd6f258584eb7ab741592c016fa565c5cc6
maksim_milykh <maksim_milykh@epam.com>
2019-01-14 10:43:24 +0300

    add docker push to travis script

---
+++ scripts/get_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# Loading environment variables
+source .envrc
+
+mkdir -p "$HOME"/.kube_gcp
+scp "$TF_VAR_ssh_user"@"$(terraform output k8s-master-ip)":.kube/config "$HOME"/.kube_gcp/config
+
+KUBECONFIG="$HOME"/.kube_gcp/config kubectl config set-cluster kubernetes --server=https://"$(terraform output k8s-master-ip)":6443
+export KUBECONFIG="$HOME"/.kube_gcp/config
+
